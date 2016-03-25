7584dd59045be480fb23b26899990a4baa6eb82e
Jon Stokes <jon@jonstokes.com>
2016-03-25 13:28:28 -0500

    Started working on row translation

--- mint2md.rb
+++ mint2md.rb
@@ -1,7 +1,6 @@
 require 'csv'
 
 module Arguments
-
   def self.valid?
     return false unless input_file && output_directory
     File.exists?(input_file) && Dir.exists?(output_directory)
@@ -24,21 +23,66 @@ module Arguments
   end
 end
 
-class Mint2Md
+class Row
+  attr_reader :date, :category
+  def initialize(line)
+    @date = line["Date"]
+    @description = line["Description"]
+    @memo = line["Original Description"]
+    @amount = line["Amount"]
+    @transaction_type = line["Transaction Type"]
+    @category = line["Category"]
+    @account_name = line["Account Name"]
+  end
 
-  def process_input_file
-    puts "Processing..."
+  def memo
+    @memo.gsub(",", " ")
+  end
+
+  def description
+    @description.gsub(",", " ")
+  end
+
+  def amount
+    return "-#{@amount}" if debit?
+    amount
+  end
+
+  private
+
+  def debit?
+    @transaction_type.downcase == "debit"
+  end
+end
+
+class Mint2Md
+  attr_reader :lines
+  attr_reader :output
+
+  def initialize(input:, output:)
+    @lines = CSV.read(input, headers: true)
+    @output_directory = output_directory
+    @output = {}
   end
 
   def run
-    if Arguments.invalid? || Arguments.help?
-      puts "usage:"
-      puts "  mint2md <input_file> <output_directory>"
-    else
-      process_input_file
+    lines.each do |line|
+      account_name = line["Account Name"]
+      @output[account_name] ||= []
+      @output[account_name] << [
+        line["Date"],
+        line["Description"],
+        line["Original Description"],
+        line["Amount"]
+      ]
     end
   end
 end
 
-Mint2Md.new.run
-
+if Arguments.invalid? || Arguments.help?
+  puts "usage:"
+  puts "  mint2md -i <input_file> -o <output_directory> -c <categories.yml>"
+else
+  processor = Mint2Md.new(input: Arguments.input_file, output: Arguments.output_directory)
+  processor.run
+end
